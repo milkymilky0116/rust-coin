907cc8a715fb25dc2cadcf5a762eaf8c106f9837
milkymilky0116 <sjlee990129@gmail.com>
2024-01-22 18:22:30 +0900

    feat: #5.3 - #5.4 Partials, Adding Block

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,60 @@
 use std::sync::Mutex;
 
 use askama::Template;
-use axum::{routing::get, Router};
+use axum::{
+    response::Redirect,
+    routing::{get, post},
+    Form, Router,
+};
 use blockchain::chain::{Block, BlockChain};
+use serde::Deserialize;
 mod blockchain;
 
 static BLOCK_CHAIN: Mutex<BlockChain> = Mutex::new(BlockChain::new());
 #[derive(Template)]
 #[template(path = "../templates/home.html")]
-struct HomeTemplate {
-    title: String,
+struct HomeTemplate<'a> {
+    title: &'a str,
     blocks: Vec<Block>,
+    _parent: &'a Layout,
 }
 
-async fn home() -> HomeTemplate {
+#[derive(Deserialize)]
+struct BlockForm {
+    block_data: String,
+}
+
+#[derive(Template)]
+#[template(path = "../templates/add.html")]
+struct AddTemplate<'a> {
+    title: &'a str,
+    _parent: &'a Layout,
+}
+
+#[derive(Template)]
+#[template(path = "../templates/layout/layout.html")]
+struct Layout {}
+
+async fn home() -> HomeTemplate<'static> {
     let blocks = BLOCK_CHAIN.lock().unwrap().all_blocks();
     HomeTemplate {
-        title: "Hello Rust".to_string(),
+        title: "Home",
         blocks,
+        _parent: &Layout {},
     }
+}
+
+async fn get_add() -> AddTemplate<'static> {
+    AddTemplate {
+        title: "Add Block",
+        _parent: &Layout {},
+    }
+}
+
+async fn post_add(Form(form): Form<BlockForm>) -> Redirect {
+    let mut chain = BLOCK_CHAIN.lock().unwrap();
+    chain.add_block(form.block_data);
+    Redirect::to("/")
 }
 
 #[tokio::main]
@@ -29,7 +65,10 @@ async fn main() {
         chain.add_block("Second Block".to_string());
         chain.add_block("Third Block".to_string());
     }
-    let app = Router::new().route("/", get(home));
+    let app = Router::new()
+        .route("/", get(home))
+        .route("/add", get(get_add))
+        .route("/add", post(post_add));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
