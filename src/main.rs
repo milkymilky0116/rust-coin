c83868d99daf82e06d3d310353b6f9547f8dd043
milkymilky0116 <sjlee990129@gmail.com>
2024-02-07 15:45:04 +0900

    feat: #7.1 Parsing commands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use std::sync::Mutex;
+use std::{process, sync::Mutex};
 
 use blockchain::chain::BlockChain;
-use rest::serve::serve;
-//use explorer::serve::serve;
 mod blockchain;
 mod explorer;
 mod rest;
 static BLOCK_CHAIN: Mutex<BlockChain> = Mutex::new(BlockChain::new());
 
+fn usage() {
+    print!("Welcome to rust-coin\n\n");
+
+    print!("Please use the following commands:\n\n");
+
+    print!("explorer:   Start the HTML Explorer\n\n");
+
+    print!("rest:   Start the REST API\n\n");
+    process::exit(1)
+}
 #[tokio::main]
 async fn main() {
     {
@@ -17,7 +25,15 @@ async fn main() {
         chain.add_block("Third Block".to_string());
     }
 
-    //serve(4000).await
+    let args = std::env::args().nth(1);
 
-    serve(3000).await
+    match args {
+        Some(arg) => match arg.as_str() {
+            "explorer" => println!("explorer"),
+            _ => println!("rest"),
+        },
+        None => usage(),
+    }
+
+    //serve(3000).await
 }
